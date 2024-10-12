83f3d08a180ba7d031095f1f66a6719c04de2ab3
Harlequelrah <degbovimax19@gmail.com>
2024-10-12 02:16:00 +0000

    mise en place de quelques evenements de reactions au DOM

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -13,13 +13,31 @@ export class FaceSnapComponent implements OnInit {
   createdAt!: Date;
   snaps!: number;
   imageUrl!: string;
+  isSnaped!: boolean;
+  snapButtonText!: "Oh Snap !" | "Oops unSnap !";
 
   ngOnInit(): void {
     this.title = 'Harlequelrah';
     this.description = 'ANGULAR DEV ';
     this.createdAt = new Date();
     this.snaps = 0;
+    this.isSnaped = false;
+    this.snapButtonText = "Oh Snap !"
     this.imageUrl = 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg';
+  }
+
+  onSnap(): void {
+    this.isSnaped = !this.isSnaped;
+    if (this.isSnaped) this.snap()
+    else this.unSnap()
+  }
+  snap(): void {
+    this.snaps++;
+    this.snapButtonText = "Oops unSnap !"
+  }
+  unSnap(): void {
+    this.snaps--;
+    this.snapButtonText = "Oh Snap !"
   }
 
 }
