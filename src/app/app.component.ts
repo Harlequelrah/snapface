d5fed97e48fd9c09618b9b2f1b9a6493c3834a63
Harlequelrah <degbovimax19@gmail.com>
2024-10-12 20:58:16 +0000

    utilisation des propitété personnalisé et d 'un modele personnalisé facesnap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FaceSnapComponent } from './face-snap/face-snap.component';
+import FaceSnap from './models/face-snap';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,37 @@ import { FaceSnapComponent } from './face-snap/face-snap.component';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'snapface';
+  mySnap!: FaceSnap;
+  otherSnap!: FaceSnap;
+  lastSnap!: FaceSnap;
+  ngOnInit(): void {
+    this.mySnap = new FaceSnap(
+      'Harlequelrah',
+      'Another pseudo of Harlequin',
+      'https://i.pinimg.com/enabled_hi/474x/03/3c/ed/033cedadad538f893a1752eaab93bda3.jpg',
+      new Date(),
+      10
+
+    )
+    this.otherSnap = new FaceSnap(
+      'Archibald',
+      'Mon meilleur ami depuis tout petit !',
+      'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+      new Date(),
+      0
+    );
+    this.lastSnap = new FaceSnap(
+      'Marine Kitagawa',
+      'Personnage de My dressup darling',
+      'https://i.pinimg.com/enabled_hi/474x/b7/18/8a/b7188a9cbe3d7ff9e35f6594661618ce.jpg',
+      new Date(),
+      100
+    )
+
+  }
+
 }
+
+
+
+
--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import FaceSnap from '../models/face-snap';
 
 @Component({
   selector: 'app-face-snap',
@@ -8,22 +9,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './face-snap.component.scss'
 })
 export class FaceSnapComponent implements OnInit {
-  title!: string;
-  description!: string;
-  createdAt!: Date;
-  snaps!: number;
-  imageUrl!: string;
+  @Input() faceSnap!: FaceSnap;
+
   isSnaped!: boolean;
   snapButtonText!: "Oh Snap !" | "Oops unSnap !";
-
   ngOnInit(): void {
-    this.title = 'Harlequelrah';
-    this.description = 'ANGULAR DEV ';
-    this.createdAt = new Date();
-    this.snaps = 0;
     this.isSnaped = false;
     this.snapButtonText = "Oh Snap !"
-    this.imageUrl = 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg';
   }
 
   onSnap(): void {
@@ -32,13 +24,15 @@ export class FaceSnapComponent implements OnInit {
     else this.unSnap()
   }
   snap(): void {
-    this.snaps++;
+    this.faceSnap.addSnap();
     this.snapButtonText = "Oops unSnap !"
   }
   unSnap(): void {
-    this.snaps--;
+    this.faceSnap.removeSnap();
     this.snapButtonText = "Oh Snap !"
   }
 
 }
+
+
 
