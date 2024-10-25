c53ca04e0ca860ed35bfb41475019b0f8a32ca3e
Harlequelrah <degbovimax19@gmail.com>
2024-10-25 01:40:51 +0000

    initiation aux observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { interval, Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent],
+  imports: [RouterOutlet,HeaderComponent,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  interval$! : Observable<number>
+  ngOnInit(): void{
+    this.interval$ = interval(1000)
+    // this.interval$.subscribe((value)=>console.log(value))
+  }
 
 
 }
