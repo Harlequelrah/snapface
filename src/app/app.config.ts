49f8bb8bf122a2a7e91e80877843e6754c091a46
Harlequelrah <degbovimax19@gmail.com>
2024-11-14 02:35:14 +0000

    utilisation d'intercepteur pour ajouter un token au headers de chaque requette

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,14 @@
 import { ApplicationConfig, LOCALE_ID, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors, withInterceptorsFromDi } from '@angular/common/http';
 import { routes } from './app.routes';
+import { httpInterceptorsProvider } from './interceptors';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
     { provide: LOCALE_ID, useValue: 'fr-FR' },
-    provideHttpClient()
+    provideHttpClient(withInterceptorsFromDi()),
+    ...httpInterceptorsProvider
 
   ]
 };
